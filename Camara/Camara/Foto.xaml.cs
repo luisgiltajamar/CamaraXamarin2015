470ea5d69f465362677dad1774c6acc28efb5827
Luis Felipe Gil Lamaignere <luis.gil@toolents.com>
2016-02-03 10:28:00 +0100

    no message

---
+++ Camara/Camara/Foto.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace Camara
+{
+    public partial class Foto : ContentPage
+    {
+        public Foto()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var f =
+                await
+                    CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+                    {
+                        Directory = "Fotos",
+                        Name = "mifoto.jpg"
+                    });
+
+            if (f != null)
+            {
+                
+                MiFoto.Source=ImageSource.FromStream(() =>
+                {
+                    var st = f.GetStream();
+                    f.Dispose();
+                    return st;
+                });
+            }
+
+
+        }
+    }
+}
